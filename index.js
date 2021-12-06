795f0c6c8a644af85d7686066caae092869bce37
wuji <mypridelife@163.com>
2021-12-06 10:08:32 +0000

    update

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // 读取typora里面的文件夹，然后把文件夹里面的文件读取出来
-//如果notion中有同名的文件，略过，如果没有，则把文件夹里面的文件替换图片地址并复制到notion中
+const fs = require('fs')
+const fileList = fs.readdirSync('./typora')
+//如果notion中有同名的文件，略过，如果没有，则把文件夹里面的文件替换图片github地址并复制到notion中
+const mdList = fileList.filter(item => item.endsWith('.md'))
+console.log('mdList', mdList)
+
+mdList.forEach(item => {
+  console.log('fs.existsSync(`./notion/${item}`)', fs.existsSync(`./notion/${item}`))
+
+  if (!fs.existsSync(`./notion/${item}`)) {
+    const filePath = `./typora/${item}`
+    const fileName = item.split('.')[0]
+    const fileContent = fs.readFileSync(filePath, 'utf8')
+    const newFileContent = fileContent.replace(
+      new RegExp(fileName + '\\/', 'g'),
+      `https://raw.githubusercontent.com/mypridelife/notion_img/main/typora/${fileName}/`
+    )
+    console.log('newFileContent', newFileContent)
+
+    fs.writeFileSync(`./notion/${item}`, newFileContent)
+  }
+})
---
+++ notion/MyBatis.md
@@ -0,0 +1,6 @@
+#### 2.3 创建 MyBatis 配置文件
+
+- 创建自定义模板：选择 resources----右键 New----Edit File Templates
+
+  ![1616144597211](https://raw.githubusercontent.com/mypridelife/notion_img/main/typora/MyBatis/1616144597211.png)
+  ![1616144597211](https://raw.githubusercontent.com/mypridelife/notion_img/main/typora/MyBatis/1616144597211.png)
